a4bb2ea201b53d92a76b64cdf843582120ccf850
Ramon Carvalho Maciel <ramongtx@gmail.com>
2015-11-03 17:55:23 -0200

    Using env variables

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -2,7 +2,7 @@ class ProfileController < ApplicationController
   def index
     if session[:userToken]
       loginData = session[:userToken]
-      apiSecret = '42e7faba26a5d2301e998a99d92a869172d3cb3ac3fb54aba432eec9b0ee064a'
+      apiSecret = ENV['API_SECRET']
       urlstring = 'https://api-staging.socialidnow.com/v1/marketing/login/info'\
       '?api_secret=' + apiSecret + '&token=' + session[:userToken]
 
