f42616cf9c1bae6cdc9c215cf4c4f30b52a933f6
Ramon Carvalho Maciel <ramongtx@gmail.com>
2015-11-05 15:53:00 -0200

    Beautify

--- spec/controllers/profile_controller_spec.rb
+++ spec/controllers/profile_controller_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 describe ProfileController do
-
   context 'GET index' do
     context 'is not logged in' do
       before do
@@ -9,18 +8,16 @@ describe ProfileController do
         get :index
       end
       subject { response }
-      it {is_expected.to redirect_to('/')}
+      it { is_expected.to redirect_to('/') }
     end
 
-
-      context 'has bad login token' do
-        before do
-          session[:loginToken] = '123123123'
-          get :index
-        end
-        subject { response }
-        it {is_expected.to redirect_to('/')}
+    context 'has bad login token' do
+      before do
+        session[:loginToken] = '123123123'
+        get :index
       end
+      subject { response }
+      it { is_expected.to redirect_to('/') }
+    end
   end
-
 end
